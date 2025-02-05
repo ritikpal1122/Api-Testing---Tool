ec26e7235aab8e1c66f2a548397042c19a649ef9
DebugDynamo <er.ritikpal@gmail.com>
2025-02-05 09:10:22 +0530

    analytics added

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
 import App from './App.tsx';
+import { Analytics } from '@vercel/analytics/react';
+
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HelmetProvider>
       <App />
+      <Analytics />
     </HelmetProvider>
   </StrictMode>
 );
